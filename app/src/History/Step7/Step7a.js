5c8bd143a3872e7210e85fa92c2554c5a1709c15
Maxime Clément <maxime.clement@smartpanda.com>
2019-03-24 17:27:39 +0100

    feat(layout) : adjustement layout

--- app/src/History/Step7/Step7a.js
+++ app/src/History/Step7/Step7a.js
@@ -18,7 +18,7 @@ class Introduction extends Component {
       image={poisson}
       reponse1="Revenir en arrière"
       choiceOne={this.choiceOne}
-      imageSize="550px"
+      imageSize="600px"
       />
     );
   }
